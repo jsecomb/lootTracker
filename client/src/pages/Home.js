916ec8e9162171c6bd02d716518da6c1b4eaa335
TBruggeman <tsbruggeman@gmail.com>
2020-07-07 12:30:37 -0700

    home page created, herocard component created, unused logos deleted

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,16 @@
 import React from "react";
-import { Container, Paper, Typography, Grid } from "@material-ui/core";
+import { Container } from "@material-ui/core";
+import { HeroCard } from "../components/HeroCard";
 
 function Home() {
 
     return (
         <>
-            <Paper>
                 <Container>
-                    <Grid container spacing={3}>
-                        <Grid item xs={12}>
-
-                            <Typography variant="h2" align="center" gutterBottom>
-                                Hello World
-                            </Typography>
-
-                        </Grid>
-                    </Grid>
-                
-            </Container>
-            </Paper>
+                    <HeroCard>
+                    </HeroCard>                
+                </Container>
+    
         </>
     )
 }
