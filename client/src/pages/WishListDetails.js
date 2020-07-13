c971e2dd6c506eb4a9197c8ff69a83148e9a5e06
Saskuatch <asmithamongus@gmail.com>
2020-07-12 17:28:43 -0700

    added line chart, started the sort, fixed index.html

--- client/src/pages/WishListDetails.js
+++ client/src/pages/WishListDetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Grid from '@material-ui/core/Grid';
 import API from "../utils/API";
-import { Doughnut } from "react-chartjs-2";
+import { Doughnut, Line } from "react-chartjs-2";
 
 function WishListDetails(props) {
     const initialDataState = {
@@ -10,28 +10,59 @@ function WishListDetails(props) {
             {
                 label: 'Total Budget Usage',
                 backgroundColor: [
-                    '#ff0505',
-                    '#17e600'
+                    '#f26005',
+                    '#787878'
                 ],
-                data:[475, 25] 
+                data: [475, 25]
             }
         ]
     };
     const [wishlist, setWishlist] = useState({});
     const [doughnutData, setDoughnutData] = useState(initialDataState);
+    const [lineData, setLineData] = useState({});
 
     useEffect(() => {
         loadWishlist();
     }, [])
+    
+    const compare = (a,b) => {
+        const gameA = a.purchaseDate;
+        const gameB = b.purchaseDate;
+        let comparison = 0
+        if (gameA > gameB) {
+            comparison = 1
+        } else if(gameA < gameB) {
+            comparison = -1
+        }
+        return comparison
+    }
 
     const loadWishlist = () => {
         API.Wishlist.getAllByUserId(props.user.id).then(res => {
             setWishlist(res.data);
             console.log(res.data);
             let amountSpent = 0;
-            res.data[0].WishlistItems.map(item => {
-                amountSpent += parseInt(item.Game.price)
+            let lineYSet = [];
+            let lineXSet = [];
+            const allGames = res.data[0].WishlistItems
+            allGames.sort(compare)
+            allGames.map(item => {
+                amountSpent += parseInt(item.Game.price);
+                let date = new Date(item.purchaseDate);
+                lineXSet.push(date.toLocaleDateString('en-US'));
+                lineYSet.push(item.Game.price);
             });
+            console.log(lineYSet)
+            setLineData({
+                labels: lineXSet,
+                datasets: [{
+                    label: 'Your spending',
+                    data: lineYSet,
+                    fill: true,
+                    backgroundColor: '#787878',
+                    borderColor: '#f26005'
+                }]
+            })
             console.log(amountSpent);
             const amountRemaining = parseInt(res.data[0].budget) - amountSpent;
             setDoughnutData({
@@ -40,10 +71,10 @@ function WishListDetails(props) {
                     {
                         label: 'Total Budget Usage',
                         backgroundColor: [
-                            '#ff0505',
-                            '#17e600'
+                            '#f26005',
+                            '#787878'
                         ],
-                        data:[amountSpent, amountRemaining] 
+                        data: [amountSpent, amountRemaining]
                     }
                 ]
             })
@@ -52,20 +83,44 @@ function WishListDetails(props) {
 
     return (
         <>
-            <Doughnut 
-                        data={doughnutData}
-                        options={{
-                            title:{
-                                display: true,
-                                text: 'Total Budget Usage',
-                                fontSize: 20
-                            },
-                            legend:{
-                                display: true,
-                                position: 'right'
+            <Doughnut
+                data={doughnutData}
+                options={{
+                    title: {
+                        display: true,
+                        text: 'Total Budget Usage',
+                        fontSize: 20
+                    },
+                    legend: {
+                        display: true,
+                        position: 'right'
+                    }
+                }}
+            />
+            <Line 
+                data={lineData}
+                options={{
+                    title: {
+                        display: true,
+                        text: 'Spending Over Time',
+                        fontSize: 20
+                    },
+                    scales: {
+                        yAxes: [
+                          {
+                            ticks: {
+                              suggestedMin: 0,
+                              suggestedMax: 120
                             }
-                        }}
-                    />
+                          }
+                        ]
+                    },
+                    legend: {
+                        display: true,
+                        position: 'right'
+                    }
+                }}
+            />
         </>
     )
 
