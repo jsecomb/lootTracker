16b763f57478a1cb0d94d6507cef3a8e165005d4
Saskuatch <asmithamongus@gmail.com>
2020-07-16 19:38:33 -0700

    made details page reload friendly

--- client/src/pages/WishListDetails.js
+++ client/src/pages/WishListDetails.js
@@ -71,8 +71,8 @@ function WishListDetails(props) {
                     label: 'Your spending',
                     data: lineYSet,
                     fill: true,
-                    backgroundColor: '#66798b',  
-                    borderColor: '#c46000', 
+                    backgroundColor: '#66798b',
+                    borderColor: '#c46000',
                 }],
             })
 
@@ -87,8 +87,8 @@ function WishListDetails(props) {
                     {
                         label: 'Total Budget Usage',
                         backgroundColor: [
-                            '#c46000',  
-                            '#66798b',  
+                            '#c46000',
+                            '#66798b',
                         ],
                         data: [amountSpent, amountRemaining]
                     }
@@ -114,7 +114,7 @@ function WishListDetails(props) {
             borderColor: '#fff',
             backgroundColor: '#424242',
             color: '#fff',
-            [theme.breakpoints.up('lg')]:{
+            [theme.breakpoints.up('lg')]: {
                 height: '150%',
                 width: '150%',
             },
@@ -124,87 +124,92 @@ function WishListDetails(props) {
 
     const classes = useStyles()
 
-    return (
-        <>
-            <Grid container spacing={3} className={classes.root}>
-                {/* <Grid item xs={false} lg={1}></Grid> */}
-                <Grid item xs={12} lg={6} id='donutgrid' className={classes.charts}>
-                    <Doughnut
-                        data={doughnutData}
-                        options={{ maintainAspectRatio: false }}
-                        options={{
-                            title: {
-                                display: true,
-                                text: 'Total Budget Usage',
-                                fontSize: 20,
-                                fontColor: '#fff',
-                            },
-                            legend: {
-                                display: true,
-                                position: 'bottom',
-                                fontColor: '#fff',
-                                width: '20%',
-                                height: '10%',
-                                labels: {
-                                    fontColor: "#fff",
-                                }
-                            },
-                        }}
-                    />
-                </Grid>
-                <Grid item xs={12} lg={6} id='lineGrid' className={classes.charts}>
-                    <Line
-                        data={lineData}
-                        id='linechart'
-                        options={{
-                            title: {
-                                display: true,
-                                text: 'Spending Over Time',
-                                fontSize: 20,
-                                fontColor: '#fff',
-                            },
-                            scales: {
-                                yAxes: [
-                                    {
-                                        ticks: {
-                                            suggestedMin: 0,
-                                            suggestedMax: [lineMax],
-                                            fontColor: '#fff',
-
-                                        },
-                                        labels: {
-                                            fontColor: '#fff',
-                                        },
-                                        dots: {
-                                            fontColor: '#fff',
-                                        },
-                                    },
-                                ],
-                                xAxes: [
-                                    {
-                                        ticks: {
-                                            fontColor: '#fff',
-                                        },
-                                        // labels: {
-                                        //     fontColor: '#fff',
-                                        // },
+    if (props.user) {
+        return (
+            <>
+                <Grid container spacing={3} className={classes.root}>
+                    {/* <Grid item xs={false} lg={1}></Grid> */}
+                    <Grid item xs={12} lg={6} id='donutgrid' className={classes.charts}>
+                        <Doughnut
+                            data={doughnutData}
+                            options={{ maintainAspectRatio: false }}
+                            options={{
+                                title: {
+                                    display: true,
+                                    text: 'Total Budget Usage',
+                                    fontSize: 20,
+                                    fontColor: '#fff',
+                                },
+                                legend: {
+                                    display: true,
+                                    position: 'bottom',
+                                    fontColor: '#fff',
+                                    width: '20%',
+                                    height: '10%',
+                                    labels: {
+                                        fontColor: "#fff",
                                     }
-                                ]
-                            },
-                            legend: {
-                                display: false,
-                                labels: {
-                                    fontColor: "#fff",
-                                }
-                            },  
-                        }}
-                    />
+                                },
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12} lg={6} id='lineGrid' className={classes.charts}>
+                        <Line
+                            data={lineData}
+                            id='linechart'
+                            options={{
+                                title: {
+                                    display: true,
+                                    text: 'Spending Over Time',
+                                    fontSize: 20,
+                                    fontColor: '#fff',
+                                },
+                                scales: {
+                                    yAxes: [
+                                        {
+                                            ticks: {
+                                                suggestedMin: 0,
+                                                suggestedMax: [lineMax],
+                                                fontColor: '#fff',
+
+                                            },
+                                            labels: {
+                                                fontColor: '#fff',
+                                            },
+                                            dots: {
+                                                fontColor: '#fff',
+                                            },
+                                        },
+                                    ],
+                                    xAxes: [
+                                        {
+                                            ticks: {
+                                                fontColor: '#fff',
+                                            },
+                                            // labels: {
+                                            //     fontColor: '#fff',
+                                            // },
+                                        }
+                                    ]
+                                },
+                                legend: {
+                                    display: false,
+                                    labels: {
+                                        fontColor: "#fff",
+                                    }
+                                },
+                            }}
+                        />
+                    </Grid>
+                    {/* <Grid item xs={false} md={1}></Grid> */}
                 </Grid>
-                {/* <Grid item xs={false} md={1}></Grid> */}
-            </Grid>
-        </>
-    )
+            </>
+        )
+    } else {
+        return(
+            <div>Loading...</div>
+        )
+    }
 }
 
-
-export default WishListDetails;
+    export default WishListDetails;
